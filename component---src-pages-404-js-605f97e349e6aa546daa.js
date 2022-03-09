4e43b6d848dd39b8a27d353724e2b8744a2672ac
bontaedev <kbt6882965@naver.com>
2022-03-09 12:48:59 +0900

    Updates

---
+++ component---src-pages-404-js-605f97e349e6aa546daa.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbontae_dev=self.webpackChunkbontae_dev||[]).push([[883],{6810:function(e,t,n){n.r(t);var u=n(7294),l=n(1280),a=n(5150);t.default=function(){return u.createElement(l.Z,null,u.createElement(a.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-605f97e349e6aa546daa.js.map
